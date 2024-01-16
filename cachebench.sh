25848de3e25b583c5f117c400d85ed9c22203be7
monishaaraman <91076694+monishaaraman@users.noreply.github.com>
2024-01-16 14:27:04 +0530

    Update cachebench.sh

--- cachebench.sh
+++ cachebench.sh
@@ -58,7 +58,7 @@ for file in "${FILES[@]}"; do
     fi
 done
 # Build Google Benchmark in the fio-3.35 directory
-DIR="$PWD/llcbench/cachebench"
+DIR="$PWD/cachebench"
 if [ -d "$DIR" ]; then
     cd "$DIR"
  # Build Google Benchmark
